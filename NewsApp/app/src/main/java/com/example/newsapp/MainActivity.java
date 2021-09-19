0b3f57b15257c23d10a66601eca8dfe4cb5b8dd2
thanh binh <1751010010binh@ou.edu.vn>
2021-09-19 17:34:01 +0700

    add login fragment, news fragment

--- NewsApp/app/src/main/java/com/example/newsapp/MainActivity.java
+++ NewsApp/app/src/main/java/com/example/newsapp/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.newsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private TabLayout mTablayout;
+    private ViewPager mViewPager;
+
+    EditText username, password;
+    Button btn_user, btn_guest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        btn_user = findViewById(R.id.btn_login_user);
+        btn_guest = findViewById(R.id.btn_login_guest);
+
+        mTablayout = findViewById(R.id.tab_id);
+        mViewPager = findViewById(R.id.view_pager);
+
+        ViewPageAdapter viewPageAdapter = new ViewPageAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        mViewPager.setAdapter(viewPageAdapter);
+        mTablayout.setupWithViewPager(mViewPager);
     }
 }
---
+++ NewsApp/app/src/main/java/com/example/newsapp/NewsFragment.java
@@ -0,0 +1,27 @@
+package com.example.newsapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link NewsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class NewsFragment extends Fragment {
+
+    public NewsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_news, container, false);
+    }
+}
