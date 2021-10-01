feec7a9604203942d935946737fccf6962e971e2
thanh binh <1751010010binh@ou.edu.vn>
2021-10-01 14:11:44 +0700

    Update link article

--- NewsApp/app/src/main/java/com/example/newsapp/fragment/ViewFragment.java
+++ NewsApp/app/src/main/java/com/example/newsapp/fragment/ViewFragment.java
@@ -38,11 +38,16 @@ public class ViewFragment extends Fragment {
 
         listItems=new ArrayList<>();
 
-        listItems.add(new The_Slide_Items_Model_Class(R.drawable.hinh1, "Tim Cook chưa 'lên đời' iPhone 13", ""));
-        listItems.add(new The_Slide_Items_Model_Class(R.drawable.hinh2, "Thế giới chạy đua công nghệ metaverse", ""));
-        listItems.add(new The_Slide_Items_Model_Class(R.drawable.hinh3, "Xiaomi tạo 'bạn gái ảo'", ""));
-        listItems.add(new The_Slide_Items_Model_Class(R.drawable.hinh4, "Chip giả tuồn vào chuỗi cung ứng", ""));
-        listItems.add(new The_Slide_Items_Model_Class(R.drawable.hinh5, "Nhân lực công nghệ đã thay đổi thế nào vì Covid-19", ""));
+        listItems.add(new The_Slide_Items_Model_Class(R.drawable.hinh1, "Tim Cook chưa 'lên đời' iPhone 13",
+                "https://vnexpress.net/tim-cook-chua-len-doi-iphone-13-4360203.html"));
+        listItems.add(new The_Slide_Items_Model_Class(R.drawable.hinh2, "Thế giới chạy đua công nghệ metaverse",
+                "https://vnexpress.net/the-gioi-chay-dua-cong-nghe-metaverse-4359951.html"));
+        listItems.add(new The_Slide_Items_Model_Class(R.drawable.hinh3, "Xiaomi tạo 'bạn gái ảo'",
+                "https://vnexpress.net/xiaomi-tao-ban-gai-ao-4359333.html"));
+        listItems.add(new The_Slide_Items_Model_Class(R.drawable.hinh4, "Chip giả tuồn vào chuỗi cung ứng",
+                "https://vnexpress.net/chip-gia-tuon-vao-chuoi-cung-ung-4358866.html"));
+        listItems.add(new The_Slide_Items_Model_Class(R.drawable.hinh5, "Nhân lực công nghệ đã thay đổi thế nào vì Covid-19",
+                "https://vnexpress.net/nhan-luc-cong-nghe-da-thay-doi-the-nao-vi-covid-19-4358393.html"));
 
         The_Slide_items_Pager_Adapter itemsPager_adapter = new The_Slide_items_Pager_Adapter(getActivity(), listItems);
         page.setAdapter(itemsPager_adapter);
